cdb08c3e3aababa7e0f62aafba6aeb542d703fd1
Dev <Dev.Sterling@outlook.com>
2026-02-25 23:49:13 +0530

    PlusOne

--- arr.py
+++ arr.py
@@ -232,5 +232,23 @@ def combinationSum( candidates: List[int], target: int) -> List[List[int]]:
     dfs(0, target, [])
     return res
 
+def plusOne(self, digits: List[int]) -> List[int]:
+    digits[-1] += 1
+    for i in range(len(digits) - 1, -1, -1):
+        if digits[i] == 10:
+            digits[i] = 0
+            if i == 0:
+                digits.insert(0, 1)
+            else:
+                digits[i - 1] += 1
+
+    return digits
+
+b = [1,2,3]
+b = sorted(b)
+c = set()
+c.add(1)
+c.add(2)
+c.add(3)
 a = combinationSum([2,3,5],8)
 print(a)
