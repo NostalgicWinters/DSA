6b0a4582e0cd5b22242e523d8a4a29fbed9da616
Dev <Dev.Sterling@outlook.com>
2026-02-26 20:17:39 +0530

    Max Average

--- slidingWindow.py
+++ slidingWindow.py
@@ -5,8 +5,10 @@
 # Common Case:- Array/String: Subarray, Substring
 
 # Variable Length
-## Longest Substring without repeating characters
+## 3. Longest Substring without repeating characters
 ### For checking if the string has repeating characters use a Set
+from typing import List
+
 
 class Solution:
     def LongestSubstring(self, s: str) -> int:
@@ -25,3 +27,25 @@ class Solution:
             sett.add(s[r])
         
         return longest
+    
+# Fixed Length
+## 643. Maximum average Subarray 1
+    def MaxAvg(self, nums: List[int], k: int) -> float:
+        
+        cur_sum = 0
+        n = len(nums)
+
+        for i in range(k):
+            cur_sum += nums[i]
+
+        max_avg = cur_sum / k
+
+        for i in range(k, n):
+            cur_sum += nums[i]
+            cur_sum -= nums[i-k]
+
+            cur_avg = cur_sum/k
+
+            max_avg = max(cur_avg, max_avg)
+        
+        return max_avg
